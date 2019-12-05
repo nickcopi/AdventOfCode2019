17645799969b200210912c71a182ae563d6f9692
nick <no@example.notreal>
2019-12-05 11:25:19 -0500

    Begin working on souped up computer.

---
+++ day5/computer.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const program = fs.readFileSync('input').toString().replace('\n','').split(',').map(instruction=>Number(instruction));
+const tape = [];
+const input = [1];
+const output = [];
+
+//program[1] = 12;
+//program[2] = 2;
+
+//console.log(JSON.stringify(program));
+
+const opCodeLookup = [];
+
+class OpCode{
+	constructor(argSize){
+		this.argSize = argSize;
+	}
+}
+
+class AddOpCode extends OpCode{
+	constructor(){
+		super(3);
+	}
+	use(inputs,tape){
+		tape[inputs[2]] = inputs[0] + inputs[1];
+	}
+}
+class MultiplyOpCode extends OpCode{
+	constructor(){
+		super(3);
+	}
+	use(inputs,tape){
+		tape[inputs[2]] = inputs[0] * inputs[1];
+	}
+}
+
+class InputOpCode extends OpCode{
+	constructor(){
+		super(1);
+	}
+	use(inputs,tape){
+		tape[inputs[0]] = input.shift();
+	}
+}
+
+class OutputOpCode extends OpCode{
+	constructor(){
+		super(1);
+	}
+	use(inputs,tape){
+		output.push(tape[inputs[0]])
+	}
+}
+
+class HaltOpCode extends OpCode{
+	constructor(){
+		super(0);
+	}
+	use(inputs,tape){
+		process.exit();
+	}
+}
+
+opCodeLookup[1] = new AddOpCode();
+opCodeLookup[2] = new MultiplyOpCode();
+opCodeLookup[3] = new InputOpCode();
+opCodeLookup[4] = new OutputOpCode();
+opCodeLookup[99] = new HaltOpCode();
+
+const parseOpCode = opCode=>{
+	opCode = [...String(opCode)];
+	let code = Number(opCode.pop());
+	if(opCode.length)
+		code = Number(opCode.pop() + String(code));
+	const argModes = [];
+	while(opCode.length > 0)
+		argModes.push(Number(opCode.pop()));
+	if(!(code in opCodeLookup)){
+		console.error(`Unknown opcode ${code}! Aborting.`);
+		process.exit();
+	}
+	if(argModes.length > opCodeLookup[code].argSize){
+		console.error(`Opcode provided modes for more inputs than it takes. ${argmodes} Aborting.`);
+		process.exit();
+	}
+	while(argModes.length < opCodeLookup[code].argSize){
+		argModes.push(0);
+	}
+	return {
+		code,
+		argModes
+	}
+}
+
+
+let pointer = 0;
+while(pointer < program.length){
+	console.log('pointer: ' + pointer);
+	const opCode = parseOpCode(program[pointer]);
+	console.log(opCode);
+	pointer += opCodeLookup[opCode.code].argSize + 1;
+
+
+}
+
